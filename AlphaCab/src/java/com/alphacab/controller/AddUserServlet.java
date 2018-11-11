5437c87d656f96950b26382b9251f1aef4fc1212
pauldavisdev <pauldavis4@gmail.com>
2018-11-11 15:58:39 +0000

    Implement add user functionality

---
+++ AlphaCab/src/java/com/alphacab/controller/AddUserServlet.java
@@ -0,0 +1,98 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.alphacab.controller;
+
+import com.alphacab.model.User;
+import com.alphacab.model.UserDao;
+import java.io.IOException;
+import java.sql.Connection;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Paul
+ */
+public class AddUserServlet extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+           RequestDispatcher view = request.getRequestDispatcher("WEB-INF/views/add_user.jsp");
+           view.forward(request, response);           
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        
+        UserDao userDao = new UserDao();
+        
+        userDao.connect((Connection)request.getServletContext().getAttribute("connection"));
+        
+        // check that username does not exist
+        
+        // check that passwords match
+
+        String username = request.getParameter("username-textbox");
+        
+        String password = request.getParameter("password-textbox");
+    
+        User newUser = new User(username, password);
+        
+        if(userDao.saveUser(newUser)) {
+            RequestDispatcher view = request.getRequestDispatcher("WEB-INF/views/add_user.jsp");
+            view.forward(request, response); 
+        } else {
+            // error user has not been added
+        }
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
--- AlphaCab/web/WEB-INF/views/add_user.jsp
+++ AlphaCab/web/WEB-INF/views/add_user.jsp
@@ -9,9 +9,15 @@
 <html>
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <title>JSP Page</title>
+        <title>Add User</title>
     </head>
     <body>
-        <h1>Hello World!</h1>
+        <h1>Welcome to the Add User Page!</h1>
+        <form method="post" action="AddUser">
+            <input type="text" name="username-textbox" placeholder="Username" required pattern="[^\s]+" title="No whitespace"><br><br>
+            <input type="password" name="password-textbox" placeholder="Password" required pattern="[^\s]+" title="No whitespace"><br><br>
+            <input type="password" name="confirm-password-textbox" placeholder="Confirm Password" required pattern="[^\s]+" title="No whitespace"><br><br>
+            <input name="register-button" type="submit" value="Add User"/>
+        </form>
     </body>
 </html>
