54cd314d2148ff1431bd992174f1b475dd103b68
pauldavisdev <pauldavis4@gmail.com>
2018-11-11 15:53:18 +0000

    Add saveUser method and change to prepared statement type

--- AlphaCab/src/java/com/alphacab/model/UserDao.java
+++ AlphaCab/src/java/com/alphacab/model/UserDao.java
@@ -6,11 +6,14 @@
 package com.alphacab.model;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -27,14 +30,45 @@ public class UserDao {
         this.connection = conn;
     }
     
+    public boolean saveUser(User user) {
+        
+        int rowsUpdated = 0;
+        
+        String query = "INSERT INTO \"Users\" VALUES (?, ?)";
+        
+        try {
+            
+            PreparedStatement statement = this.connection.prepareStatement(query);
+            statement.setString(1, user.getUsername().trim());
+            statement.setString(2, user.getPassword().trim());
+            rowsUpdated = statement.executeUpdate();
+            if (rowsUpdated > 0) {
+                return true;
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(UserDao.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        return false;
+    }
+    
     public boolean doesUserExist(User user) throws SQLException {
         
-        Statement statement = this.connection.createStatement();
+        ResultSet resultSet;
         
-        ResultSet resultSet = statement.executeQuery("SELECT * FROM \"Users\" WHERE \"username\" = '" + user.getUsername() +"' AND \"password\" = '" + user.getPassword() + "'");
-
-        if(resultSet.next()) {
-            return true;
+        String query = "SELECT * FROM \"Users\" WHERE \"username\" = ? AND \"password\" = ?";
+        
+        try {
+            PreparedStatement statement = this.connection.prepareStatement(query);
+            statement.setString(1, user.getUsername());
+            statement.setString(2, user.getPassword());
+            resultSet = statement.executeQuery();
+            
+            if(resultSet.next()) {
+                return true;
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(UserDao.class.getName()).log(Level.SEVERE, null, ex);
         }
         
         return false;
