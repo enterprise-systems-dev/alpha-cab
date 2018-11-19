0bc1ceecdd601dcc7dfb7ab0a9ec4ec7c95075d5
pauldavisdev <pauldavis4@gmail.com>
2018-11-19 12:21:54 +0000

    Add id and role properties with getters and setters

--- AlphaCab/src/java/com/alphacab/model/User.java
+++ AlphaCab/src/java/com/alphacab/model/User.java
@@ -11,14 +11,34 @@ package com.alphacab.model;
  */
 public class User {
     
+    private int id;
     private String username;
     private String password;
+    private String role;
+
+    public User() {
+        
+    }
 
     public User(String username, String password) {
         this.username = username;
         this.password = password;
     }
+    
+    public User(String username, String password, String role) {
+        this.username = username;
+        this.password = password;
+        this.role = role;
+    }
 
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+    
     public String getUsername() {
         return username;
     }
@@ -34,6 +54,13 @@ public class User {
     public void setPassword(String password) {
         this.password = password;
     }
-    
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
     
 }
