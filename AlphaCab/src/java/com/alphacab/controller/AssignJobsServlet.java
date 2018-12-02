9fa6d70922e7f9420924bbe10a9660c7b6621da1
marcusCarpenter97 <marcus.carpenter97@yahoo.co.uk>
2018-12-02 14:07:33 +0000

    servlet to display outstanding jobs and available drivers, jsp and a change in driver class required

---
+++ AlphaCab/src/java/com/alphacab/controller/AssignJobsServlet.java
@@ -0,0 +1,91 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.alphacab.controller;
+
+import com.alphacab.model.UserDao;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author marcus
+ */
+public class AssignJobsServlet extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+    }
+
+ 
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+        // Display outstanding demands and available drivers
+        
+        RequestDispatcher view = request.getRequestDispatcher("index.jsp");
+        
+        UserDao userDao = new UserDao();
+        
+        request.setAttribute("outstandingDemands", userDao.getAllDemands());
+        
+        request.setAttribute("availableDrivers", userDao.getAvailableDrivers());
+                        
+        view.forward(request, response);        
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+        // Give selected driver the selected demand
+        
+        RequestDispatcher view = request.getRequestDispatcher("index.jsp");
+        
+        view.forward(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
--- AlphaCab/src/java/com/alphacab/controller/CallNooberServlet.java
+++ AlphaCab/src/java/com/alphacab/controller/CallNooberServlet.java
@@ -36,7 +36,7 @@ public class CallNooberServlet extends HttpServlet {
             throws ServletException, IOException {
     }
 
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+
     /**
      * Handles the HTTP <code>GET</code> method.
      *
